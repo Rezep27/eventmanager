0faa109ecc7562044f08958aee0e7caf81a5971f
Jose <109845879+JxRiera@users.noreply.github.com>
2023-11-13 10:25:09 -0500

    test (volt)

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const {isEmail} = require('validator')
+const {isEmail} = require('validator') //holacomoestas
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: [true, 'Please enter an email'], unique: true, lowercase: true, validator: [isEmail, 'Please enter a valid email']},
